a78854ddb81a314a1e925611133803ca34a6588a
Hasan Yurdakul <65603504+hasanyurdakul@users.noreply.github.com>
2024-08-09 16:55:24 +0300

    Update HeroSection.jsx

--- src/components/Landing/HeroSection.jsx
+++ src/components/Landing/HeroSection.jsx
@@ -16,7 +16,7 @@ function HeroSection() {
           <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
             <a
               href="/"
-              className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              className="bg-secondary hover:bg-secondaryHover inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
               <TrendingUpIcon className="w-3 h-3 mr-4" />
               Teklif Alın
